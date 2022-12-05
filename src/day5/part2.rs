904451227916c6c5636da6289356c251e70cf89b
Jon Winsley <jon.winsley@gmail.com>
2022-12-05 21:04:04 +0000

    Day 5 solution

---
+++ src/day5/part2.rs
@@ -0,0 +1,67 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+
+pub fn run(input: &str) -> String {
+    let (stacks, commands) = input.split("\n\n").next_tuple().unwrap();
+
+    let stack_names: Vec<char> = stacks.split('\n').last().unwrap().trim().chars().skip(0).step_by(4).collect();
+
+    let mut stacks_map: HashMap<char, Vec<char>> = HashMap::new();
+
+    // Initialize stacks
+    for stack in stack_names.clone() {
+        stacks_map.insert(stack, Vec::new());
+    }
+
+    // Populate stacks
+    for row in stacks.split('\n') {
+        if !row.contains('[') {
+            continue;
+        }
+        stack_names
+            .clone()
+            .iter()
+            .zip(
+                row
+                    .chars()
+                    .skip(1)
+                    .step_by(4)
+            )
+            .filter(|(_, c)| !c.is_whitespace())
+            .for_each(|(stack, item)| 
+                stacks_map
+                    .get_mut(stack)
+                    .unwrap()
+                    .insert(0, item)
+                );
+    }
+
+
+    // parse commands
+    for command in commands.split('\n') {
+        let (_, count, _, from, _, to) = command.split(' ').next_tuple().unwrap();
+        
+        let from_char = from.chars().next().unwrap();
+        let to_char = to.chars().next().unwrap();
+        let count_number: usize = count.parse().unwrap();
+        
+        let from_list = stacks_map.get_mut(&from_char).unwrap();
+        let elements: Vec<char> = from_list.drain(from_list.len() - count_number..).collect();
+        let to_list = stacks_map.get_mut(&to_char).unwrap();
+        to_list.extend_from_slice(&elements);
+    }
+
+    stack_names.iter().map(|stack| stacks_map.get(stack).unwrap().iter().last().unwrap()).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        let demo = include_str!("inputs/sample.txt");
+        assert_eq!(run(demo), "MCD");
+    }
+}
