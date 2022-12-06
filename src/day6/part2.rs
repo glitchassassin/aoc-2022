4640f05bd1693e7a3dc21f5255691ec1fd7bcbea
Jon Winsley <jon.winsley@gmail.com>
2022-12-06 13:52:32 +0000

    Day 6 solution

---
+++ src/day6/part2.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+
+pub fn run(input: &str) -> usize {
+    let size = 14;
+    for (index, window) in input.chars().collect::<Vec<char>>().windows(size).enumerate() {
+        if window.iter().collect::<HashSet<&char>>().len() == size {
+            return index + size;
+        }
+    }
+    
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        let demo = include_str!("inputs/sample.txt");
+        assert_eq!(run(demo), 19usize);
+    }
+}
